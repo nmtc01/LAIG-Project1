7d3dd721a72661cafb6bd47c06a209373e47478c
Nuno <ns.cardoso@hotmai.com>
2019-10-03 20:52:42 +0100

    Commented init cameras

--- LAIGDemoParser20192020v1.1/XMLscene.js
+++ LAIGDemoParser20192020v1.1/XMLscene.js
@@ -160,7 +160,7 @@ class XMLscene extends CGFscene {
 
         this.setGlobalAmbientLight(this.graph.ambient[0], this.graph.ambient[1], this.graph.ambient[2], this.graph.ambient[3]);
 
-        this.initCameras();
+        //this.initCameras();
 
         this.initLights();
 
